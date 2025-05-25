51422408c0c9b9e8389834342707344e30cdca15
Ydavies <hiro.davies@gmail.com>
2025-05-26 09:42:44 +1000

    Q1.6 complete

---
+++ Part1.06.js
@@ -0,0 +1,10 @@
+// linearSearch.js
+const myArray= [11, 5, 8, 3, 25, 16, 31, 45, 14, 20];
+console.log(myArray);
+
+// Creating the search function
+
+  // we need to find the value
+  let Result = 2;
+  console.log(`Result: ${Result}`);
+
