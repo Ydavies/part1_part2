34eabc4da9838adbd18f322764fe15711d149310
Ydavies <hiro.davies@gmail.com>
2025-05-26 10:42:51 +1000

    additional info
    add  year and rating

--- Part2/part2.js
+++ Part2/part2.js
@@ -8,3 +8,13 @@ console.log(myMovie);
 myMovie.rating = 5;
 console.log(myMovie);
 
+// Add in Summary: World War II movie about the Normandy landings
+myMovie.summary = "World War II movie about the Normandy landings";
+console.log(myMovie);
+
+//  Change the year to 1962
+myMovie.year = 1962;
+console.log(myMovie);
+// Change the rating to 4
+myMovie.rating = 4;
+console.log(myMovie);
